d204918c32f808332a3ec01cd0b878c469e70b6d
tetriv0217 <tetriv0217@gmail.com>
2024-10-02 00:14:59 +0530

    Done a question on average

---
+++ Chapter 2/5_average.py
@@ -0,0 +1,5 @@
+a = int(input("Enter your first number : "))
+b = int(input("Enter your second number : "))
+
+print("the average is ",int((a+b)/2)) # floor
+print("the average is ",((a+b)/2)) # fraction
