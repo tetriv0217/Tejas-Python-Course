0f2c635755b8592cb3fccca20eb58dbc3f89ee1c
tetriv0217 <tetriv0217@gmail.com>
2024-10-10 18:14:59 +0530

    made some changes in code

--- Chapter 9 File IO/Chapter 9 PS/q5.py
+++ Chapter 9 File IO/Chapter 9 PS/q5.py
@@ -2,7 +2,7 @@ ls = ["Donkey","Pussy","Fuck"]
 with open("Chapter 9 File IO/Chapter 9 PS/q4_donkey.txt","r+") as f:
     content = f.read()
     for word in ls:
-        content = content.replace(word,"#####").replace(word.lower(),"######")
+        content = content.replace(word,"#"*len(word)).replace(word.lower(),"#"*len(word))
     f.seek(0)
     f.write(content)
     f.truncate()
